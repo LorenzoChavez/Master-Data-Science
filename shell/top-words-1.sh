4db941e6ecba78ba2856d52172427929ce6624de
Lorenzo Chavez <lorenzo.cht@gmail.com>
2018-03-31 18:20:02 +0200

    adding script

---
+++ shell/top-words-1.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+#1. command below will extract a list of the most used words in a .txt file
+
+cat $2 | tr '[:upper:]' '[:lower:]' | grep -oE '\w+' | sort | uniq -c | sort -nr | head -n $1
