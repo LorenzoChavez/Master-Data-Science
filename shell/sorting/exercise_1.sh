4390e89922b20a9648b75efb72455be33ada1162
Lorenzo Chavez <lorenzo.cht@gmail.com>
2018-04-01 13:45:06 +0200

    adding exercise

--- shell/sorting/exercise_1.sh
+++ shell/sorting/exercise_1.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-# Find top 10 files by size in your current directory including the subdirectories. Sort them by size and print the result including the size and the name of the file (hint: use find with –size and -exec ls –s parameters)
+# Find top 10 files by size in your current directory including the subdirectories. 
+# Sort them by size and print the result including the size and name of the file 
+# (hint: use find with –size and -exec ls –s parameters)
 
 find ./ -exec ls -s {} + | sort -n -r -u | head
 
