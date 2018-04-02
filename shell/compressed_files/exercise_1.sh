9780ba2fa6564e228442c1a0a31d20c122262e67
Lorenzo Chavez <lorenzo.cht@gmail.com>
2018-04-02 19:52:57 +0200

    adding exercises

---
+++ shell/compressed_files/exercise_1.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Go to ~/Data/us_dot/otp. Show the content of one of the files.
+
+zcat ~/Data/us_dot/otp/On_Time_On_Time_Performance_2015_1.zip | less
---
+++ shell/compressed_files/exercise_2.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Use head/tail together with zcat command. Any difference in time execution?
+
+zcat ~/Data/us_dot/otp/On_Time_On_Time_Performance_2015_1.zip | head
