a55a360d3cb3cf73f062493732cf4c3cb0f7a29e
Lorenzo Chavez <lorenzo.cht@gmail.com>
2018-05-15 23:10:26 +0200

    added last exercises

--- R/exercises1_20180512.R
+++ R/exercises1_20180512.R
@@ -1,4 +1,4 @@
-#1. Mean of the age of men that survived
+# 1. Mean of the age of men that survived
 aggregate(Age~Sex == "male" & Survived == 1, titanic, mean)
 mean(titanic[titanic$Sex == "male" & titanic$Survived, "Age"], na.rm = TRUE) #another way
 
@@ -8,26 +8,26 @@ titanic %>%   # another way
   .[,"Age"] %>%
   mean(na.rm = TRUE) #(media = mean(Age, na.rm = TRUE))
 
-#2. How many people survived
+# 2. How many people survived
 sum(titanic$Survived)
 
-#3. How many people died
+# 3. How many people died
 sum(!titanic$Survived)
 
-#4. How many people travelled on the Titanic
+# 4. How many people travelled on the Titanic
 nrow(titanic)
 sum(titanic$Embarked != "")
 
-#5. Ratio between 1st Class and 3rd Class passengers
+# 5. Ratio between 1st Class and 3rd Class passengers
 firstClass <- sum(titanic$Pclass == 1)
 thirdClass <- sum(titanic$Pclass == 3)
 
 thirdClass / firstClass
 
-#6. Select columns Age and Sex for 1st class passengers
+# 6. Select columns Age and Sex for 1st class passengers
 titanic[titanic$Pclass == 1,c("Age", "Sex")]
 
-#7. Calculate the mask to select 3rd Class passengers who survived 
+# 7. Calculate the mask to select 3rd Class passengers who survived 
 #   or men from 1st Class who died
 survived3rd <- titanic$Pclass == 3 & titanic$Survived
 died1st <- titanic$Pclass == 1 & !titanic$Survived & titanic$Sex == 'male'
@@ -37,8 +37,11 @@ firstOr3rd <- survived3rd | died1st
 
 nrow(titanic[firstOr3rd, ])
 
-#8. Correlation between age and fare grouped by sex
-cor(titanic[ , c("Age", "Fare")], use = "comple")
+# 8. Correlation between age and fare grouped by sex
+women <- titanic[titanic$Sex == 'female', ]
+men <- titanic[titanic$Sex == 'male', ]
+cor(women[ , c("Age", "Fare")], use = "comple")
+cor(men[ , c("Age", "Fare")], use = "comple")
 
 ## 9. Create a new column in "titanic" named risk with the values below:
 #            Woman    Man
@@ -64,10 +67,16 @@ titanic$Risk[men1st] <- 2
 titanic$Risk[men2nd] <- 3
 titanic$Risk[men3rd] <- 4
 
-## 10. Crea otra columna desviacionFare con la diferencia entre Fare y la media
-# de todos los "Fare" del dataset.
-# Calcula el mínimo y máximo de esta nueva variable para cada combinación
-# de sexo y clase.
+# 10. Create another column named deviationFare theat contains the difference
+# between Fare and the mean of all "Fare" from the dataset.
+# Also, calculate the min and max of this new variable for each pair, sex and class.
+
+titanic$deviationFare <- 0
+fareMean <- mean(titanic$Fare)
+
+titanic$deviationFare <- titanic$Fare - fareMean
+min(titanic$deviationFare)
+max(titanic$deviationFare)
 
 ## 11. Mira la ayuda de xtabs y presenta los mínimos de otra manera con
 # esta función
