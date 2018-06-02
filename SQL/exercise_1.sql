00a482b74ee2f0adf0909b2fb7dd10ac959be90f
Lorenzo Chavez <lorenzo.cht@gmail.com>
2018-06-02 23:13:02 +0200

    updated

---
+++ SQL/exercise_1.sql
@@ -0,0 +1,14 @@
+/*Create 'friends' DB if it does not exist.*/
+/*Generate 'Facebook' table where every person is identified with the name, age,
+city of residence, and email. The table should at least have 10 rows*/
+/*Write a query to obtain Facebook friends which are younger than <18 or older
+than 65 years.*/
+/*Write a query to obtain all the facebook friends which do not reside in Madrid.*/
+
+CREATE DATABASE friends;
+
+CREATE TABLE facebook (name VARCHAR, age INT, city VARCHAR, email VARCHAR);
+
+SELECT * FROM my_fb_friends WHERE age <18 OR age>65;
+
+SELECT * FROM my_fb_friends WHERE residence NOT LIKE 'Madrid';
